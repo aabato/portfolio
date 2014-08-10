59abf568ad03f390801474958b7f3e2486e73b0b
Angelica Bato <angelica.bato@gmail.com>
2014-08-10 08:59:45 -0400

    Edited animations.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,16 @@
-$main = $('.main');
+var $main = $('.main');
+var $first = $('h1, nav');
+var $last = $('h2');
 
-$main.animate({
+$first.animate({
 	opacity: 1
-},2000);
+}, 2000);
+
+$last.delay(1000).animate({
+	opacity: 1
+}, 2000);
+
+
+
+
 
